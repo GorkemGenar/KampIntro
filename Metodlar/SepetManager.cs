2dab705579fc751ece919ac8b32088626f820c6b
Görkem Genar Akkaya <gorkem_akkaya@hotmail.com>
2021-01-22 18:05:55 +0300

    Proje dosyası ekle.

---
+++ Metodlar/SepetManager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Metodlar
+{
+    class SepetManager
+    {
+        public void Ekle(Urun urun)
+        {
+            Console.WriteLine(urun.Adi + " Sepete Eklendi.");
+        }
+
+        public void Ekle2(string urunAdi, string Aciklama, double Fiyat, int stokAdedi)
+        {
+            Console.WriteLine(urunAdi + " Sepete Eklendi.");
+        }
+    }
+}
---
+++ OOP1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace OOP1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product product1 = new Product();
+            product1.Id = 1;
+            product1.CategoryId = 2;
+            product1.ProductName = "Masa";
+            product1.UnitPrice = 500;
+            product1.UnitsInStock = 3;
+
+            Product product2 = new Product
+            {
+                Id = 1,
+                CategoryId = 2,
+                ProductName = "Masa",
+                UnitPrice = 500,
+                UnitsInStock = 3
+            };
+
+            ProductManager productManager = new ProductManager();
+            productManager.Add(product1);
+            Console.WriteLine(product1.ProductName);
+
+
+            
+            // int, double, bool, ... => Değer Tip
+            // diziler, class, abstract class, interface, ... => Referans Tip
+
+            Console.ReadLine();
+        }
+    }
+}
